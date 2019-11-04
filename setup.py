f2dddffa6cb484f36a8a02dc7f112517ab039e00
tadly <git@tadly.de>
2019-11-04 08:35:12 +0100

    version bump to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lwe-pyloader',
-    version='1.0.1',
+    version='1.1.0',
     description='A simple, easy to use, multithreaded downloader '
                 'with queuing.',
     long_description='For an overview and some examples, head over to '
